3b0f19eeeb1301f45a3072e5a91d617c9896800c
veggiss <veg_ulf@hotmail.com>
2017-10-04 05:56:58 +0200

    Added camera shake under activation

--- src/objects/Door.js
+++ src/objects/Door.js
@@ -1,13 +1,28 @@
 class Door extends Phaser.Sprite {
-	constructor(game, x, y, 'button') {
+	constructor(game, x, y) {
+		super(game, x, y, 'door');
+		//Globals
+		this.activated = false;
+
 		//Sprite
 		this.autoCull = true;
+		this.anim = this.animations.add('complete');
+		this.anim.onComplete.add(this.activateDoor, this);
 
 		//Physics
 		game.physics.arcade.enable(this);
 		this.body.immovable = true;
 		this.body.moves = false;
 	}
+
+	activateDoor() {
+		this.activated = true;
+	}
+
+	startAnim() {
+		this.anim.play(12);
+		this.game.camera.shake(0.0005, 1000);
+	}
 }
 
-export default Button;
+export default Door;
