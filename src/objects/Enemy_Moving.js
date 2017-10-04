79ca973b24805eb7127eb939b2d0b5cf0ff50b38
veggiss <veg_ulf@hotmail.com>
2017-10-04 05:57:40 +0200

    Updated enemies to move opposite direction on collision

--- src/objects/Enemy_Moving.js
+++ src/objects/Enemy_Moving.js
@@ -6,33 +6,35 @@ class Enemy_Moving extends Phaser.Sprite {
 
 		this.game = game;
 
-		//Globals
-		this.movingEnemyTick = 120;
-		this.movingEnemySwitch = false;
-
-
 		//Sprite
 		this.animations.add('walking', [0, 1, 2, 3, 4, 5, 6, 7], 8, true);
 		this.animations.play('walking');
 		this.anchor.setTo(0.5, 0.5);
 		this.autoCull = true;
+		
+		if(Math.random() >= 0.5) {
+			this.scale.x = 1;
+		}
 
 		//Physics
 		game.physics.arcade.enable(this);
 		this.body.immovable = true;
-		this.scale.x = -1;
 	}
 
 	update() {
 		if (this.alive) {
 			if (this.body.blocked.left || this.body.touching.left) {
 				this.scale.x = 1;
-			} else if (this.body.blocked.right || this.body.touching.left) {
+			} else if (this.body.blocked.right || this.body.touching.right) {
 				this.scale.x = -1;
 			}
 
 			this.body.velocity.x = this.scale.x * 12;
 		}
+	}
+
+	testFunc() {
+		console.log("LOL");
 	}
 }
 
