0bbdbfc250f5f3b0d92f4960538cfa5aa0eb691f
veggiss <veg_ulf@hotmail.com>
2017-10-17 03:13:57 +0200

    Added small delay before mea can carry Birger

--- src/objects/Mea.js
+++ src/objects/Mea.js
@@ -17,6 +17,7 @@ class Mea extends Phaser.Sprite {
 		this.juiceMax = 10000;
 		this.juice = this.juiceMax;
 		this.isFlying = false;
+		this.gravDelay = 10;
 		this.gravKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
 		//Globals
@@ -25,8 +26,8 @@ class Mea extends Phaser.Sprite {
 		Mea.getAlive = this.alive;
 
 		//Sprite
-		this.animations.add('flapping', [0, 1, 2, 3, 2, 1, 0], 28, true);
-		this.animations.play('flapping');
+		this.flyAnim = this.animations.add('flapping', [0, 1, 2, 3, 2, 1, 0]);
+		this.flyAnim.play(15, true);
 		this.anchor.setTo(0.5, 0.5);
 		this.smoothed = false;
 
@@ -65,22 +66,30 @@ class Mea extends Phaser.Sprite {
 
 			this.game.physics.arcade.moveToXY(this, Player.getX - (Player.getScaleX * 7), Player.getY - this.meaY, 60, 150);
 		} else {
-			this.game.physics.arcade.moveToXY(this, Player.getX + (Player.getBody.velocity.x/8), Player.getY - 13, 30, 100);
+			this.game.physics.arcade.moveToXY(this, Player.getX + (Player.getBody.velocity.x/8), Player.getY + (Player.getBody.velocity.y/8) - 8, 30, 100);
 		}
 		this.scale.x = -Player.getScaleX;
 	}
 
 	antiGravity() {
 		if (this.gravKey.isDown && this.juice > 1) {
-			if (Player.getBody.velocity.y > -50) {
+			this.flyAnim.delay = 20;
+			if (this.gravDelay > 0) {
+				this.gravDelay--;
+			}
+			
+			if (Player.getBody.velocity.y > -50 && this.gravDelay < 1) {
 				Player.getBody.velocity.y -= 15;
 			}
+
 			this.juice--;
 			this.noGravTime = this.game.time.now;
 			this.juiceMeter.resetMeter(0, 0);
 			this.isFlying = true;
 		} else {
+			this.flyAnim.delay = 35;
 			this.isFlying = false;
+			this.gravDelay = 10;
 			if (this.juice < this.juiceMax && !this.gravKey.isDown && this.game.time.now - this.noGravTime > this.juiceTimer) {
 				this.juice++;
 			}
